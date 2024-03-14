10df71e09fefc61cf71429cd03d2a463d870a654
Hayden <hctitans032600@gmail.com>
2024-03-14 00:01:09 -0500

    PC to laptop

--- asg06/QueueManager.hpp
+++ asg06/QueueManager.hpp
@@ -2,7 +2,9 @@
 #include <queue>
 #include <iostream>
 #include <fstream>
-#include <stdexcept>
+#include <stdexcept>    //Plan to throw exc if queue enum gets set to invalid value
+
+
 enum Q {FORE = 0, BACK, BATCH};
 
 class QueueManager
@@ -10,14 +12,25 @@ class QueueManager
     private:
         //RR q=1
         std::priority_queue<job> foreground;
+
         //FCFS q=2, q=3
-        std::queue<job> background, 
-                        batch;
-        int maxPass[3] = {1, 2, 3};
+        std::queue<job> background;
+        std::queue<job> batch;
+
+        //MaxPass and Quantum members
+        int maxPass= 2;
+        ms quantum = ms(200);
+
     public:
         QueueManager()
         {
             //set default maxpass and quantum
+            maxPass = 2;
+            quantum = ms(200);
+        }
+        ~QueueManager()
+        {
+
         }
         void enter(job j)
         {
@@ -27,23 +40,43 @@ class QueueManager
         void reschedule(job j)
         {
             j.pass++;
-            if(j.pass >= maxPass[j.currentQueue])
+            if(j.pass >= maxPass)
             {
                 //RESET PASS TO 0
+                j.pass = 0;
+
                 //INC QUEUE IF < BATCH
+                if(j.currentQueue == FORE)
+                    j.currentQueue = BACK;
+                else if (j.currentQueue == BACK)
+                    j.currentQueue = BATCH;
             }
             switch(j.currentQueue)
             {
                 //CASES
                 //PUSH TO APPROPRIATE QUEUE
+                case FORE:
+                    enter(j);
+                    break;
+                case BACK:
+                    background.push(j);
+                    break;
+                case BATCH:
+                    batch.push(j);
+                    break;
+
+                //Only three cases should be possible; default is very bad news
                 default:
                     throw std::out_of_range("Queue enum is invalid");
             }
         }
 
+        
         job* schedule()
         {
-            job* out = nullptr;
+            //Processor thread handles deletion of ptr
+            job* out = new job;
+
             if(!foreground.empty())
             {
                 *out = foreground.top();
@@ -64,6 +97,7 @@ class QueueManager
                 delete out;
                 return nullptr;
             }
+
             return out;
         }
 
--- asg06/ScheduleSim.cpp
+++ asg06/ScheduleSim.cpp
@@ -1,6 +1,7 @@
 #include <queue>
 #include <chrono>
 #include <random>
+#include <mutex>
 #include "QueueManager.hpp"
 
 using ms = std::chrono::milliseconds;
@@ -8,7 +9,8 @@ using ms = std::chrono::milliseconds;
 std::random_device rand{};
 std::mt19937 generator{rand()};
 std::normal_distribution<unsigned int> nD{2000, 500};
-std::uniform_int_distribution uD(0,9);
+std::uniform_int_distribution uD(0, 9);
+std::poisson_distribution pD(3);
 
 struct job
 {
@@ -28,5 +30,29 @@ struct job
 
 int main()
 {
+    //THREE THREADS
 
+        //MAIN
+            //Initialize
+            QueueManager qm;
+            //Run
+            
+            //Report stats
+
+        //JOB CREATION
+            //Creates new job 
+            //CRITICAL:
+                //Insert into foreground
+            //Sleeps
+
+        //PROCESSOR
+            //CRITICAL
+                //Checks queues for work
+
+            //Sleep for x quantums
+
+            //CRITICAL
+                //Update runtimes and idle times for each job
+                //Reschedule job if not complete
+                //else: update global totals for report and delete job
 }
